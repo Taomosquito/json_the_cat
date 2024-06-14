b9bf13f3672ad526f2c6512463b587ee4bbbcf88
Taomosquito <S_Peloquin123@live.com>
2024-06-14 13:58:53 -0400

    create testing resources use mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -17,23 +17,41 @@ const needle = require("needle");
 const temporaryQueryflag = process.argv;
 let breedPortion;
 
-if(temporaryQueryflag.length === 3) {
+if (temporaryQueryflag.length === 3) {
   breedPortion = temporaryQueryflag[2];
 }
 
-const fetcher = function (breedPortion) {
-    needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedPortion}`, `utf8`, (error, response) => {    
+// Keeping this as it is a viable means of accomplishing this task when callbacks aren't required.
+const fetcher = function(breedPortion) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedPortion}`, `utf8`, (error, response) => {
     if (Array.isArray(response.body) && response.body.length > 0) {
-      if(!error){
+      if (!error) {
         console.log(response.body[0].description);
       } else {
         console.log(`An unexpected error occurred: ${error.message}`);
       }
-    }
-    else {
+    } else {
       console.log('Breed not found');
-    }  
-  })
-}
+    }
+  });
+};
 
-fetcher(breedPortion)
+// Below is the function we will be using for the assignment:
+const fetchBreedDescription = function(breedName, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, 'utf8', (error, response) => {
+    if (error) {
+      callback(error, null);
+
+    } else if (Array.isArray(response.body) && response.body.length > 0) {
+      const description = response.body[0].description;
+      callback(null, description);
+
+    } else {
+      callback('Breed not found', null);
+    }
+  });
+};
+
+fetcher(breedPortion);
+
+module.exports = { fetchBreedDescription, fetcher };
